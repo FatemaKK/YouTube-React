93ae557c530ef9e6de055c005ff47edffed7ed8f
Fatema Kabir <fatema.kabir@pursuit.org>
2021-12-08 12:01:11 -0500

    about-section

---
+++ src/Components/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class About extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <p>Bryant 
+            <br/>
+            Fatema - Pursuit Fellow | Full Stack Web Development
+            <br/>
+            Joshua
+            <br/>
+            Sev</p>
+        <p>React application that lets you search for YouTube videos using the YouTube API, then choose a video from a list to watch. Recreated parts of the YouTube application.</p>
+        <a href="https://github.com/BryantMercedes212/YouTube-React">GitHub</a> 
+      </div>
+    );
+  }
+}
+export default About;
