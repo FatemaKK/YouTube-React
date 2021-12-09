e77feb0dd7d564af6d26b3ef6451e9ff151fe048
Bryant Mercedes <bryantmercedescanela@gmail.com>
2021-12-09 12:51:01 -0500

    lol

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ import About from "./Components/About";
 import Home from "./Components/Home";
 import NavBar from "./Components/NavBar";
 
-
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-
       <div className="App">
         <NavBar />
         <Routes>
