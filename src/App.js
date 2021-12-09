19c3a54b94a8eb3be2633a0f423ac63e470c572b
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-12-09 13:49:35 -0500

    adds & updates startpage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import "./App.css";
-
 import { Component } from "react";
 import { Routes, Route } from "react-router-dom";
 import About from "./Components/About";
 import Home from "./Components/Home";
+import Video from "./Components/Video";
 import NavBar from "./Components/NavBar";
+import StartPage from "./Components/StartPage";
+import Footer from "./Components/Footer";
 
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-
       <div className="App">
         <NavBar />
         <Routes>
-          <Route path="/About" element={<About />} />
+          <Route exact path="/" element={<StartPage />} />
           <Route path="/Home" element={<Home />} />
+          <Route path="/Videos/:id" element={<Video />} />
+          <Route path="/About" element={<About />} />
         </Routes>
+        <Footer />
       </div>
     );
   }
