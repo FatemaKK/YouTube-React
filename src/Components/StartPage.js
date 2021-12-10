dba0dc2e0e29fa170046e3f1b586613a755ce38a
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-12-10 15:15:45 -0500

    adds start page styling

--- src/Components/StartPage.js
+++ src/Components/StartPage.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./StartPage.css";
 
 const StartPage = () => {
-  return <p> Welcome! Let's go home and search some videos!</p>;
+  return <p className="welcome"> Welcome to the best YouTube Clone on the market, let's go home and search some videos!</p>;
 };
 
 export default StartPage;
