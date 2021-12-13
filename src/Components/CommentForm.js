8fce8db4888a8ab684a682b731e7ae0015984844
Sara Perkaj <saraperkaj@pursuit.org>
2021-12-13 12:08:49 -0500

    didnt touch anything in the video.js but i made a line of empty space by accident :p.AND! the comments section is done ((:

--- src/Components/CommentForm.js
+++ src/Components/CommentForm.js
@@ -1,16 +1,77 @@
 import { Component } from "react";
+// import Comments from "./Comments";
 
 class CommentForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      commentInput: "",
+      commenterName: "",
+      comments: [],
+    };
+  }
+
+  handleSubmit = (event) => {
+    const { comments, commentInput, commenterName } = this.state;
+    event.preventDefault();
+    this.setState({
+      comments: [
+        ...comments,
+        { name: commenterName, commentInput: commentInput },
+      ],
+      commenterName: "",
+      commentInput: "",
+    });
+  };
+
+  handleNameChange = (event) => {
+    this.setState({
+      commenterName: event.target.value,
+    });
+  };
+
+  handleInputChange = (event) => {
+    this.setState({
+      commentInput: event.target.value,
+    });
+  };
+
   render() {
+    const { comments } = this.state;
+
+    const eachComment = comments.map((comment) => {
+      return (
+        <li>
+          <h3>{comment.name} says:</h3>
+          <p>{comment.commentInput}</p>
+        </li>
+      );
+    });
     return (
       <div>
-        <input className="Person-Name" type="text" placeholder="name" />
-        <input
-          className="comment-box"
-          type="text"
-          placeholder="comment something here..."
-        />
-        <button type="submit">Comment</button>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="name"
+            className="Person-Name"
+            type="text"
+            value={this.state.commenterName}
+            onChange={this.handleNameChange}
+            placeholder="name"
+            required
+          />
+          <input
+            name="comment"
+            className="comment-box"
+            type="text"
+            value={this.state.commentInput}
+            onChange={this.handleInputChange}
+            placeholder="comment something here..."
+            required
+          />
+          <button type="submit">Comment</button>
+        </form>
+        <ul>{eachComment}</ul>
       </div>
     );
   }
